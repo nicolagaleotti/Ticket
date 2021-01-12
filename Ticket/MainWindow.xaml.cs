8b05d7261629c7e1592db3561d74e3289738db96
Nicola Galeotti <niccio2802@outlook.it>
2021-01-12 13:16:24 +0100

    implementato bottone aggiungi cliente

--- Ticket/MainWindow.xaml.cs
+++ Ticket/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TicketCP;
 
 namespace Ticket
 {
@@ -23,6 +24,53 @@ namespace Ticket
         public MainWindow()
         {
             InitializeComponent();
+
+            btnM.IsChecked = true;
+        }
+
+        private void btnAggiungiCliente_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string nome;
+                string cognome;
+
+                if (txtNome.Text != "")
+                {
+                    nome = txtNome.Text;
+                }
+                else throw new Exception("Inserire un nome!");
+
+                if(txtCognome.Text != "")
+                {
+                    cognome = txtCognome.Text;
+                }
+                else throw new Exception("Inserire un cognome!");
+
+                Cliente cliente = new Cliente(nome, cognome);
+
+                cliente.SetCellulare(txtCellulare.Text);
+                if(btnM.IsChecked == true)
+                {
+                    cliente.SetSesso(true);
+                }
+                else
+                {
+                    cliente.SetSesso(false);
+                }
+
+                cmbClienti.Items.Add(cliente.Stampa());
+                cmbClienti1.Items.Add(cliente.Stampa());
+
+                txtNome.Clear();
+                txtCognome.Clear();
+                txtCellulare.Clear();
+                btnM.IsChecked = true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
---
+++ TicketCP/Cliente.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketCP
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        private string cellulare;
+        private string sesso;
+
+        public Cliente(string nome, string cognome)
+        {
+            Nome = nome;
+            Cognome = cognome;
+        }
+
+        public string GetCellulare()
+        {
+            return cellulare;
+        }
+
+        public void SetCellulare(string cellulare)
+        {
+            if (cellulare.Length == 10)
+            {
+                try
+                {
+                    int c = int.Parse(cellulare);
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Inserire soltanto numeri nel campo 'cellulare'!");
+                }
+            }
+            else throw new Exception("Il campo 'cellulare' deve contenere esattamente 10 numeri!");
+        }
+
+        public string GetSesso()
+        { 
+            return sesso;
+        }
+
+        public void SetSesso(bool s)
+        {
+            if (s == true)
+            {
+                sesso = "M";
+            }
+            else sesso = "F";
+        }
+
+        public string Stampa()
+        {
+            return $"{Nome} {Cognome}";
+        }
+    }
+}
